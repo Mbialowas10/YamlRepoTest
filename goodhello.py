8c917c48cb507f384652cee5049fd450a68362d8
Michael Bialowas <mbialowas@rrc.ca>
2025-12-19 10:02:17 -0600

    added new files.

---
+++ goodhello.py
@@ -0,0 +1,20 @@
+"""
+ This module prints greetings using simple functions.
+"""
+
+def hello():
+    """
+    Prints a simple greeting.
+    """
+    print("Hello, DevOps learner!")
+
+def bye():
+    """
+    Docstring for bye
+    """
+    print("Goodbye!")
+
+
+if __name__ == "__main__":
+    hello()
+    bye()
